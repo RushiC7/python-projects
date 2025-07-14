e3217dc43f1f741531234859202495199d67ca87
Rushikesh Chavan <rushiexp99@gmail.com>
2025-07-14 09:55:47 +0530

    Update TradeJournalAdv.py

--- TradeJournalAdv.py
+++ TradeJournalAdv.py
@@ -1,14 +1,1 @@
-import numpy as np
 
-trades = np.array([
-    [100, 110],[95, 90],[105, 120]])
-profit=trades[:, 1] - trades[:, 0]
-profitArray=np.array(profit)
-Avg=np.mean(profitArray)
-MaxP=np.max(profitArray)
-print(f"Win: {len(profitArray[profitArray > 0])}")
-print(f"Loss: {len(profitArray[profitArray < 0])}")
-print(f"Win Rate: {len(profitArray[profitArray > 0]) / len(profitArray) * 100:.2f}%")
-print(f"Profit: {profitArray}")
-print(f"Average Profit: {Avg}")
-print(f"Maximum Profit: {MaxP}")
